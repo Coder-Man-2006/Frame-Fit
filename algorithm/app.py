a1a234332eeac59c51a7d22532bbdb2bdc2e8011
Coder-Man-2006 <jatoujoseph@gmail.com>
2023-08-26 15:37:59 -0400

    Sucessfully Measured the Face

--- algorithm/app.py
+++ algorithm/app.py
@@ -1,6 +1,7 @@
 import cv2
 import mediapipe as mp
 import numpy as np
+import pandas as pd
 import os
 
 class FaceMap:
@@ -50,7 +51,7 @@ class FaceAnalyzer:
                right_eye_indices + top_lip_indices + bottom_lip_indices
                
     def detect_face_shape(self, filtered_landmarks):
-        distances = self.calculate_distances(filtered_landmarks)
+        distances = self.calculate_distances(filtered_landmarks, 10)
         non_none_distances = [d for d in distances if d is not None]
 
         if len(non_none_distances) > 1:
@@ -60,13 +61,34 @@ class FaceAnalyzer:
         else:
             return "Shape Detection Not Possible"
 
-    def calculate_distances(self, filtered_landmarks):
+    # def calculate_distances(self, filtered_landmarks):
 
+    #     distances = []
+
+    #     for point1, point2 in zip(filtered_landmarks, filtered_landmarks[1:]):
+    #         distance = np.sqrt((point2[0] - point1[0]) ** 2 + (point2[1] - point1[1]) ** 2)
+            
+    #         distances.append(distance)
+
+    #     return distances
+    
+    def calculate_distances(self, filtered_landmarks, ppi):
         distances = []
 
-        for point1, point2 in zip(filtered_landmarks, filtered_landmarks[1:]):
-            distance = np.sqrt((point2[0] - point1[0]) ** 2 + (point2[1] - point1[1]) ** 2)
-            
+        # List of landmark indices for different measurements
+        length_of_face_indices = [10, 9, 8, 6, 5, 4, 1, 2, 0, 12, 14, 15, 16, 175]
+        temple_to_temple_indices = [68, 104, 69, 119, 299, 333, 298]
+        cheekbone_to_cheekbone_indices = [280, 371, 281, 51, 110, 50]
+        jawline_indices = [366, 361, 288, 397, 365, 379, 378, 369, 377, 120, 176, 117, 118, 168, 172, 58, 164, 93, 234]
+
+        for indices in [length_of_face_indices, temple_to_temple_indices, cheekbone_to_cheekbone_indices, jawline_indices]:
+            distance = 0.0
+            for i in range(len(indices) - 1):
+                point1 = filtered_landmarks[indices[i]]
+                point2 = filtered_landmarks[indices[i + 1]]
+                pixel_distance = np.sqrt((point2[0] - point1[0]) ** 2 + (point2[1] - point1[1]) ** 2)
+                real_distance = pixel_distance / ppi
+                distance += real_distance
             distances.append(distance)
 
         return distances
@@ -74,22 +96,43 @@ class FaceAnalyzer:
     # ... (other methods like detect_face_shape and draw_landmarks remain the same)
     
     def draw_landmarks(self, frame, filtered_landmarks, distances): 
-            print("Distances:", distances)  # Print the distances array to debug
-            for landmark in filtered_landmarks:
-                cv2.circle(frame, landmark, 2, (0, 0, 255), -1)
+           # Draw lines for specific measurements
+        if distances[0] is not None:
+            cv2.line(frame, filtered_landmarks[0], filtered_landmarks[8], (0, 255, 0), 2)  # Green line for "Length of Face"
+        if distances[1] is not None:
+            cv2.line(frame, filtered_landmarks[68], filtered_landmarks[104], (0, 255, 0), 2)  # Green line for "Temple to Temple"
+        if distances[2] is not None:
+            cv2.line(frame, filtered_landmarks[69], filtered_landmarks[119], (0, 255, 0), 2)  # Green line for "Cheekbone to Cheekbone"
+        if distances[3] is not None:
+            length_of_face = [10, 9, 8, 6, 5, 4, 1, 2, 0, 12, 14, 15, 16, 175] # List of Points across length of face
+            temple_to_temple = [68, 104, 69, 119, 299, 333, 298]
+            cheekbone_to_cheekbone = [280, 371, 281, 51, 110, 50]
+            jawline_points = [366, 361, 288, 397, 365, 379, 378, 369, 377, 120, 176, 117, 118, 168, 172, 58, 164, 93, 234]
+            for i in range(len(length_of_face) - 1):
+                cv2.line(frame, filtered_landmarks[length_of_face[i]], filtered_landmarks[length_of_face[i + 1]], (0, 255, 0), 2)
+            for i in range(len(temple_to_temple) - 1):
+                cv2.line(frame, filtered_landmarks[temple_to_temple[i]], filtered_landmarks[temple_to_temple[i + 1]], (0, 255, 0), 2)
+            for i in range(len(cheekbone_to_cheekbone) - 1):
+                cv2.line(frame, filtered_landmarks[cheekbone_to_cheekbone[i]], filtered_landmarks[cheekbone_to_cheekbone[i + 1]], (0, 255, 0), 2)
+            for i in range(len(jawline_points) - 1):
+                cv2.line(frame, filtered_landmarks[jawline_points[i]], filtered_landmarks[jawline_points[i + 1]], (0, 255, 0), 2)
+                
+        print("Distances:", distances)  # Print the distances array to debug
+        for landmark in filtered_landmarks:
+            cv2.circle(frame, landmark, 2, (0, 0, 255), -1)
 
-            if distances[0] is not None:
-                cv2.putText(frame, f"Length of Face: {distances[0]:.2f}", (20, 100), cv2.FONT_HERSHEY_PLAIN,
-                            2, (255, 0, 0), 2)
-            if distances[1] is not None:
-                cv2.putText(frame, f"Temple to Temple: {distances[1]:.2f}", (20, 150), cv2.FONT_HERSHEY_PLAIN,
-                            2, (255, 0, 0), 2)
-            if distances[2] is not None:
-                cv2.putText(frame, f"Cheekbone to Cheekbone: {distances[2]:.2f}", (20, 200), cv2.FONT_HERSHEY_PLAIN,
-                            2, (255, 0, 0), 2)
-            if distances[3] is not None:
-                cv2.putText(frame, f"Jawline: {distances[3]:.2f}", (20, 250), cv2.FONT_HERSHEY_PLAIN,
-                            2, (255, 0, 0), 2)
+        if distances[0] is not None:
+            cv2.putText(frame, f"Length of Face: {distances[0]:.2f}", (20, 100), cv2.FONT_HERSHEY_PLAIN,
+                        2, (255, 0, 0), 2)
+        if distances[1] is not None:
+            cv2.putText(frame, f"Temple to Temple: {distances[1]:.2f}", (20, 150), cv2.FONT_HERSHEY_PLAIN,
+                        2, (255, 0, 0), 2)
+        if distances[2] is not None:
+            cv2.putText(frame, f"Cheekbone to Cheekbone: {distances[2]:.2f}", (20, 200), cv2.FONT_HERSHEY_PLAIN,
+                        2, (255, 0, 0), 2)
+        if distances[3] is not None:
+            cv2.putText(frame, f"Jawline: {distances[3]:.2f}", (20, 250), cv2.FONT_HERSHEY_PLAIN,
+                        2, (255, 0, 0), 2)
             
             face_shape = self.detect_face_shape(filtered_landmarks)
             cv2.putText(frame, f"Face Shape: {face_shape}", (20, 300), cv2.FONT_HERSHEY_PLAIN,
@@ -100,6 +143,9 @@ class FaceAnalyzer:
         screenshot_folder = "gallery"
         os.makedirs(screenshot_folder, exist_ok=True)
         screenshot_count = 1
+        
+        measurement_columns = ["Length of Face", "Temple to Temple", "Cheekbone to Cheekbone", "Jawline"]
+        measurement_df = pd.DataFrame(columns=measurement_columns)
         
         while True:
             key = cv2.waitKey(1)
@@ -114,10 +160,14 @@ class FaceAnalyzer:
                     face_landmarks = [(int(landmark.x * frame.shape[1]), int(landmark.y * frame.shape[0]))
                                     for landmark in faceLms.landmark]
                     filtered_landmarks = [face_landmarks[i] for i in self.all_indices]
-                    distances = self.calculate_distances(filtered_landmarks)
+                    distances = self.calculate_distances(filtered_landmarks, 10)
                     self.draw_landmarks(frame, filtered_landmarks, distances)
                     
                     total_distance = 0.0
+            
+            measurements = [distances[0], distances[1], distances[2], distances[3]]
+            measurement_df.loc[len(measurement_df)] = measurements
+
                 
             if key == ord('q'):
                 break
@@ -130,6 +180,9 @@ class FaceAnalyzer:
                 # Convert the total distance to centimeters using the conversion factor
                 # real_world_distance = total_distance * conversion_factor
                 # print(f"Real-world distance: {real_world_distance:.2f} cm")
+            
+        measurement_df.to_csv("measurements.csv", index=False)
+
 
         self.cap.release()
         cv2.destroyAllWindows()
@@ -137,3 +190,4 @@ class FaceAnalyzer:
 if __name__ == "__main__":
     face_analyzer = FaceAnalyzer()
     face_analyzer.run()
+
