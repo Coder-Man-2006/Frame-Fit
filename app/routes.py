86c798d77ad342ab1e2d432fd3139dd99cb2d4f9
nexus_x86 <60083031+nexus-x86@users.noreply.github.com>
2023-08-25 21:44:58 -0400

    Template for our application

---
+++ app/routes.py
@@ -0,0 +1,19 @@
+from flask import render_template, request, url_for
+from app import app
+from PIL import Image, ImageDraw
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    img_path = None
+
+    if request.method == 'POST':
+        number = int(request.form['number'])
+        
+        img = Image.new('RGB', (100, 30), color = (73, 109, 137))
+        d = ImageDraw.Draw(img)
+        d.text((10,10), str(number), fill=(255, 255, 0))
+
+        img_path = "static/images/number.png"
+        img.save(f"app/{img_path}")
+        
+    return render_template('index.html', img_path=img_path)
