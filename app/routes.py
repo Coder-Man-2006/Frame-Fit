1cf641c3731d4acd87a6761b4607ca82674a43eb
Coder-Man-2006 <jatoujoseph@gmail.com>
2023-08-26 11:34:58 -0400

    organized program into classes

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
-from flask import render_template, request
+from flask import Flask, render_template, request
 from app import app
 from PIL import Image, ImageDraw
+from flask_socketio import SocketIO
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -22,3 +23,30 @@ def index():
 def run_python_code():
     print('webcam active: true')
     return 'Python code executed successfully'
+
+
+app = Flask(__name__)
+socketio = SocketIO(app)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@socketio.on('connect')
+def handle_connect():
+    print('WebSocket client connected')
+
+@socketio.on('disconnect')
+def handle_disconnect():
+    print('WebSocket client disconnected')
+
+if __name__ == '__main__':
+    socketio.run(app, debug=True)
+    
+    
+    
+    
+    
+    
+    
+    
