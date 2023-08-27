13db083fedf57773018757a9519f6770b974112b
Coder-Man-2006 <jatoujoseph@gmail.com>
2023-08-26 20:42:29 -0400

    reverted

--- app/routes.py
+++ app/routes.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 from flask import render_template, request, jsonify, Flask
 from PIL import Image, ImageDraw
 from flask_socketio import SocketIO, emit
@@ -5,49 +6,41 @@ import base64
 import io
 from PIL import Image
 from algorithm.thealgo import FaceAnalyzer
-
+=======
+from flask import Flask, render_template, request
 from app import app
+from PIL import Image, ImageDraw
+from flask_socketio import SocketIO
+>>>>>>> parent of 0e012dd (integrated frontend and backend)
 
-socketio = SocketIO(app)
-
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    img_path = None
+
+    if request.method == 'POST':
+        number = int(request.form['number'])
+        
+        img = Image.new('RGB', (100, 30), color = (73, 109, 137))
+        d = ImageDraw.Draw(img)
+        d.text((10,10), str(number), fill=(255, 255, 0))
+
+        img_path = "static/images/number.png"
+        img.save(f"app/{img_path}")
+        
+    return render_template('index.html', img_path=img_path)
 
 @app.route('/run_python_code', methods=['POST'])
 def run_python_code():
     print('webcam active: true')
     return 'Python code executed successfully'
 
-@app.route('/process_frame', methods=['POST'])
-def process_frame():
-    data_url = request.form['frame']
-    header, encoded = data_url.split(",", 1)
-    binary_data = base64.decodebytes(encoded.encode())
-    
-    try:
-        image = Image.open(io.BytesIO(binary_data))
-        print("Image format:", image.format)
-    except Exception as e:
-        print("Error opening image:", e)
-        return jsonify({'error': 'Failed to process the image.'})
-    
-    try:
-        face_analyzer = FaceAnalyzer()
-        processed_image = face_analyzer.process_single_image(image)
-    except Exception as e:
-        print("Error processing image:", e)
-        return jsonify({'error': 'Failed to process the image.'})
 
-    try:
-        buffered = io.BytesIO()
-        processed_image.save(buffered, format="PNG")
-        processed_base64_image = base64.b64encode(buffered.getvalue()).decode('utf-8')
-    except Exception as e:
-        print("Error encoding image:", e)
-        return jsonify({'error': 'Failed to encode the image.'})
-    print("Returning image)")
-    return jsonify({'processed_image': f'data:image/png;base64,{processed_base64_image}'})
+app = Flask(__name__)
+socketio = SocketIO(app)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 @socketio.on('connect')
 def handle_connect():
@@ -60,6 +53,7 @@ def handle_disconnect():
 if __name__ == '__main__':
     socketio.run(app, debug=True)
     
+<<<<<<< HEAD
 
 # Initialize Flask app and SocketIO
 app = Flask(__name__)
@@ -94,3 +88,12 @@ def process_webcam_frame(frame_data):
         emit('processed_frame', {'processed_image': f'data:image/png;base64,{processed_base64_image}'})
     except Exception as e:
         print("Error processing frame:", e)
+=======
+    
+    
+    
+    
+    
+    
+    
+>>>>>>> parent of 0e012dd (integrated frontend and backend)
